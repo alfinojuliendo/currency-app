dcdb4abb51bdebbf5f0b2d2331770a41ff947e40
Alfino Juliendo <alfino.juliendo@mokapos.com>
2021-10-08 22:26:43 +0700

    initial setup

---
+++ app/src/main/java/com/demo/currency/presentation/activity/DemoActivity.kt
@@ -0,0 +1,12 @@
+package com.demo.currency.presentation.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.demo.currency.R.layout
+
+class DemoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(layout.activity_demo)
+    }
+}
